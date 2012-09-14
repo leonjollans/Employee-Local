a87ea246e79493bc773223f66212b04d9d59d8b9
Leon Jollans <leonjollans@gmail.com>
2012-09-14 03:24:26 +0100

    logging?

--- www/js/log.js
+++ www/js/log.js
@@ -1,10 +1,17 @@
 window.tempLog = [];
+var messagepending = false;
 
 window.Log = function (message) {
     console.log(message);
-    tempLog.push(message);
-};
-
-window.getLog = function () {
-    return tempLog;
+    if ($ && $('#debug-info').length > 0) {
+        for (var idx in tempLog) {
+            $('#debug-info').append('<li>LOG: ' + tempLog[idx] + '</li>');
+        }
+        messagepending = false;
+        $('#debug-info').append('<li>LOG: ' + message + '</li>');
+    }
+    else {
+        tempLog.push(message);
+        messagepending = true;
+    }
 };
